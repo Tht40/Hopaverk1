02b4504255ee51cff484f43258b56cd81ca9fc19
Tht40 <77277019+Tht40@users.noreply.github.com>
2022-03-15 15:13:43 +0000

    init

---
+++ sql/insert.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO users (name, username, password, admin) VALUES ('admin adminsson','admin', '$2a$11$pgj3.zySyFOvIQEpD7W6Aund1Tw.BFarXxgLJxLbrzIv/4Nteisii','1');
+INSERT INTO users (name, username, password, admin) VALUES ('user userson','user', '$2a$11$pgj3.zySyFOvIQEpD7W6Aund1Tw.BFarXxgLJxLbrzIv/4Nteisii','0');
+
