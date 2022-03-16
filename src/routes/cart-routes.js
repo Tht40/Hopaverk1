df6f634a3d2c5e104e24405e2816be3556498d64
Tht40 <77277019+Tht40@users.noreply.github.com>
2022-03-16 23:58:33 +0000

    cart inprogress

---
+++ src/routes/cart-routes.js
@@ -0,0 +1,32 @@
+/*
+import express from 'express';
+import { catchErrors } from '../lib/catch-errors.js';
+
+export const cartRouter = express.Router();
+
+
+
+async function eventRoute(req, res, next) {
+  const { slug } = req.params;
+  const cart = await listCart(slug);
+
+  if (!cart) {
+    return next();
+  }
+
+  return cart  total price og öllum items í cart ;
+}
+
+
+cartRouter.get('/cart/:slug', catchErrors(eventRoute));
+cartRouter.post('/cart/:slug', (req, res) => {
+
+  const { }
+});
+cartRouter.delete('/cart/:slug',);
+
+cartRouter.get('cart/:cartid/line/:id',);
+cartRouter.patch('cart/:cartid/line/:id',);
+cartRouter.delete('cart/:cartid/line/:id',);
+
+*/
