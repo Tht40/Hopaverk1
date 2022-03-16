a58106ce00e473b1501e27f5daca0faef1d1e577
Steinar Logi <steinarlg@gmail.com>
2022-03-16 21:49:36 +0000

    lagaði setup

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
+CREATE EXTENSION
+IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE public.items
 (
-=======
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-CREATE TABLE public.items (
->>>>>>> 4f60c3d066b2f3b97353171f7d0f8908751cfe5d
   id SERIAL PRIMARY KEY,
   Title VARCHAR(64) NOT NULL UNIQUE,
   Price INTEGER NOT NULL UNIQUE,
@@ -18,7 +15,6 @@ CREATE TABLE public.items (
   WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-<<<<<<< HEAD
   CREATE TABLE public.categoreis
   (
     id SERIAL PRIMARY KEY,
@@ -27,14 +23,6 @@ CREATE TABLE public.items (
     event INTEGER NOT NULL,
     created TIMESTAMP
     WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
-=======
-CREATE TABLE public.categories (
-  id SERIAL PRIMARY KEY,
-  Title VARCHAR(64) NOT NULL UNIQUE,
-  comment TEXT,
-  event INTEGER NOT NULL,
-  created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
->>>>>>> 4f60c3d066b2f3b97353171f7d0f8908751cfe5d
 
 );
 
@@ -47,7 +35,6 @@ CREATE TABLE public.categories (
       admin bit NOT NULL
     );
 
-<<<<<<< HEAD
     CREATE TABLE public.cart
     (
       id uuid PRIMARY KEY default uuid_generate_v4(),
@@ -62,17 +49,6 @@ CREATE TABLE public.categories (
         WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
  name VARCHAR
         (64) NOT NULL
-=======
-CREATE TABLE public.cart (
-  id uuid DEFAULT uuid_generate_v4 (),
-  created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE public.order (
-  id uuid DEFAULT uuid_generate_v4 (),
-  created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  name VARCHAR(64) NOT NULL
->>>>>>> 4f60c3d066b2f3b97353171f7d0f8908751cfe5d
 );
 
 
